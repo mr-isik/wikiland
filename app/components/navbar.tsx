75a2cedfa76351307aa90514b7028e11a0e7d59c
Ömer Faruk IŞIK <faruk_isik_24@protonmail.com>
2023-11-06 22:20:14 +0200

    changed name

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="w-full px-2 sm:px-6 py-4 flex items-center justify-between bg-zinc-800 gap-2 sm:gap-4 flex-col sm:flex-row">
       <h1 className="text-2xl font-bold text-white">
-        <Link href="/">WikiRocket</Link>
+        <Link href="/">WikiLand</Link>
       </h1>
       <Search />
     </nav>
